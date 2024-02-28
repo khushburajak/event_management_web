1e99a3653e44166e2b4ea3937b5701fdcdaafb14
khushburajak <khushburajak17@gmail.com>
2024-02-28 21:56:36 +0545

    created event and user model

---
+++ src/main/java/com/example/webassignment/event_management/model/User.java
@@ -0,0 +1,45 @@
+package com.example.webassignment.event_management.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // You might want to add more fields like roles, email, etc.
+}
